58c3020d8c89913f049a4a8c40c5d9a4fa05a9b4
JlordS32 <jlordsty128@gmail.com>
2024-12-29 15:48:46 +1100

    added dynamic sprite layer sort

--- Assets/Scripts/Background/DynamicSort.cs
+++ Assets/Scripts/Background/DynamicSort.cs
@@ -1,16 +1,45 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class DynamicSort : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private string _sortingLayerName = "Background";
+
+    // References
+    private TilemapRenderer[] _children;
+
+    private void Awake()
     {
-        
+        // Initialise Game Object array
+        int childCount = transform.childCount;
+        _children = new TilemapRenderer[childCount];
+
+        for (int i = 0; i < childCount; i++)
+        {
+            _children[i] = transform.GetChild(i).GetComponent<TilemapRenderer>();
+        }
+
+        // Start sorting
+        SortLayerBasedOnIndex(_children);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SortLayerBasedOnIndex(TilemapRenderer[] objects)
     {
-        
+        int sortingOrder = objects.Length; 
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i] != null)
+            {
+                // Assign sorting layer name
+                objects[i].sortingLayerName = _sortingLayerName;
+
+                // Assign sorting order (decreasing)
+                objects[i].sortingOrder = sortingOrder;
+                sortingOrder--;
+            }
+        }
     }
+
 }
